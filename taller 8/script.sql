4cdbddce08a045108c9a8b128014d6f88a7946ec
Luciano Portales <lportales655@gmail.com>
2021-10-08 15:10:20 -0300

    taller bbdd

--- taller 8/script.sql
+++ taller 8/script.sql
@@ -18,8 +18,19 @@ $$
 	begin
 		camposMod := concat(old.*);
 		camposInsertados := concat(new.*);
-		INSERT INTO transacciones values (TG_TABLE_NAME, TG_OP, now(), user, camposInsertados, camposMod);
-		return new;
+		if TG_OP = 'DELETE' then
+			INSERT INTO transacciones values (TG_TABLE_NAME, TG_OP, now(), user, null, camposMod);
+			return old;
+		end if;
+		if TG_OP = 'INSERT' then
+			INSERT INTO transacciones values (TG_TABLE_NAME, TG_OP, now(), user, camposInsertados, null);
+			return new;
+		end if;
+		if TG_OP = 'UPDATE' then
+			INSERT INTO transacciones values (TG_TABLE_NAME, TG_OP, now(), user, camposInsertados, camposMod);
+			return new;
+		end if;
+		return null;
 	end;
 $$ language plpgsql;
 
@@ -43,3 +54,78 @@ update propiedades set superficie = 230 where dueno = '20319956-2' and tipo_prop
 delete from propiedades where dueno = '20319956-2';
 
 select * from transacciones;
+
+-- ejercicio 2 --
+--creación tabla ventasxmes
+--creación tabla ventasxmes
+DROP TABLE if exists ventasxmes;
+create table ventasxmes(
+	mesAnio timestamp not null,
+	vendedor varchar not null,
+	transaccion varchar not null,
+	cant_transaccion varchar,
+	monto_transaccion varchar
+);
+
+
+--funcion trigger
+create or replace function sumarizacion() returns trigger as
+$$
+	begin
+		insert into ventasxmes (mesAnio,vendedor,transaccion,cant_transaccion,monto_transaccion)
+			values(now(),current_user,new.tipo_operacion,new.id_propiedad,new.precio);
+		return(new);	
+	end;
+$$
+language plpgsql;
+
+create trigger upd_precios after update on operaciones
+	for each row
+	execute procedure sumarizacion();
+
+update operaciones set precio = '43432'
+where id_propiedad = '29' and comprador ='28883432-6';
+
+select * from operaciones where id_propiedad='29'; 
+select * from ventasxmes;
+
+-- ejercicio 3 --
+drop function  if exists validarPropiedad cascade;
+create or replace function validarPropiedad() returns trigger as
+$$	
+	declare
+		provinc varchar;
+		tipo varchar;
+	begin
+		provinc :=  provincia from provincias where new.provincia = id_provincia;
+		tipo :=  tipo_propiedad from tipos_propiedades where new.tipo_propiedad = id_tipo;
+
+		if (provinc = 'Lleida' and tipo = 'Parking' and new.superficie >= 100) then	
+			return new;
+		elsif (provinc = 'Tarragona' and tipo = 'Casa' and new.superficieconstruida <= new.superficie) then
+			return new;
+		elsif (provinc = 'Girona' and tipo = 'Suelo' and new.superficie >= 200) then
+			return new;
+		elsif (provinc = 'Barcelona' and new.superficieconstruida <= new.superficie * 2 )then
+			return new;
+		elsif (tipo = 'Industrial' and new.superficie >= 500) then
+			return new;
+		else 
+			raise exception 'El registro ingresado no cumple con las restricciones establecidas por los reguladores.';
+		end if;
+		return new;
+	end;
+$$ language plpgsql;
+
+-- creacion del trigger de validacion --
+drop trigger if exists validar_propiedad on propiedades;
+create trigger validar_propiedad before 
+	insert on propiedades
+	for each row
+	execute procedure validarPropiedad();
+
+insert into propiedades (superficie, superficieconstruida, dueno, tipo_propiedad, provincia) values (500, 40, '20319956-2', 1, 2);
+
+
+
+
